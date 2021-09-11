fd3603ff1f720e62febbd4d9e59f446f418d1485
tareqnmd <tareqnmd@gmail.com>
2021-09-11 16:08:02 +0600

    index.js added

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log(5);
+/**
+ * This is a function.
+ *
+ * @param {number} num1 - A number param
+ * @param {number} num2 - A number param
+ * 
+ * @return {number} Add numbers
+ *
+ * @example
+ *
+ *     addTwoNumber(15,18);
+ */
+function addTwoNumber(num1, num2) {
+    return num1 + num2;
+}
+
+const sum = addTwoNumber(15, 18);
+
+console.log(sum)
