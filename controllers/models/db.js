e4258320b8b49a3a7380981cddf571d26dd86141
tareqnmd <tareqnmd@gmail.com>
2021-09-20 22:31:37 +0600

    4th class

--- controllers/models/db.js
+++ controllers/models/db.js
@@ -1,15 +1,16 @@
 const mysql = require('mysql');
 const Bluebird = require('bluebird');
+require('dotenv').config();
 
 const config = {
-    host: '127.0.0.1',
-    port: '3306',
-    user:'root',
-    password: '',
-    database:'accountManager'
-}
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME
+};
 
 const connection = mysql.createConnection(config);
-connection.query = Bluebird.promisify(connection.query)
+connection.query = Bluebird.promisify(connection.query);
 
 module.exports = connection;
